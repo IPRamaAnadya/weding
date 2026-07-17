3992520d0f08cf82f9d30ad0a7344e93771db671
I Putu Rama Anadya <gusrama18@gmail.com>
2026-07-18 07:48:18 +0800

    update

---
+++ src/lib/seo.ts
@@ -0,0 +1,9 @@
+export const siteUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+
+export const defaultSeoImage = {
+  url: '/images/rama/og.webp',
+  width: 1200,
+  height: 630,
+  type: 'image/webp',
+  alt: 'The Wedding of Rama & Mita',
+}
